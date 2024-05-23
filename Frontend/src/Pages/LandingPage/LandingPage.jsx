68a2c7b6a848c5e28cbc965ee100c4a555ae8740
vikash chand <vikash.chand2020@vitstudent.ac.in>
2024-05-23 11:49:29 +0530

    video

--- Frontend/src/Pages/LandingPage/LandingPage.jsx
+++ Frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react';
-import {  FaSearchengin, FaSearch,FaUpload, FaUser, FaRegNewspaper, FaMoneyCheckAlt, FaLinkedinIn } from 'react-icons/fa';
+import {  FaSearchengin, FaSearch,FaUpload, FaUser, FaRegNewspaper,FaVideo,FaHandsHelping, FaMoneyCheckAlt, FaLinkedinIn } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { GiTeacher } from "react-icons/gi";
 import './LandingPage.css';
@@ -68,20 +68,30 @@ const LandingPage = () => {
         <div className="feature-description">
         
           <ul className="feature-cards">
-            <li className="feature-card">
-              <div className="feature-icon">
-                <FaUpload />
-              </div>
-              <div className="feature-details">
-                <h3> Upload</h3>
-                
-                <p>automatically extracts the text from image hence ,user wont need to type subject name ,code faculty name, just You need to be a good at taking snaps</p>
-              
-                <button >   <Link className='lin' to="/qpupload">Upload </Link> </button>
-              
+        
+          
+    
+
+
+          <li className="feature-card">
+          <div className="feature-icon">
+            <FaUpload />
+          </div>
+          <div className="feature-details">
+            <h3> Upload</h3>
             
-              </div>
-            </li>
+            <p>automatically extracts the text from image hence ,user wont need to type subject name ,code faculty name, just You need to be a good at taking snaps</p>
+          
+            <button >   <Link className='lin' to="/qpupload">Upload </Link> </button>
+          
+        
+          </div>
+        </li>
+
+
+
+
+
 
             <li className="feature-card">
               <div className="feature-icon">
@@ -123,7 +133,31 @@ const LandingPage = () => {
             </div>
         </li>
 
-        
+        <li className="feature-card">
+        <div className="feature-icon">
+          <FaHandsHelping />
+        </div>
+        <div className="feature-details">
+          <h3>Seniors Advice</h3>
+  
+          <p>Tips for placements by Placed seniors</p>
+          <button >   <Link className='lin' to="/placementBlogs">Review </Link> </button>
+            
+          </div>
+      </li>
+
+         
+        <li className="feature-card">
+        <div className="feature-icon">
+        <FaVideo/>
+        </div>
+        <div className="feature-details">
+       
+          <h3>Demo</h3>
+          <iframe className='video' src="https://www.youtube.com/embed/f41Y-V1nIlk?si=5AIHI2_Xrknep6Pv&amp;controls=0&amp;start=5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+      </li>
+      
 
       
           
