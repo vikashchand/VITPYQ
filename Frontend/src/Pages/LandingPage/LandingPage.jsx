b32bc6740f747378da26183e92084d4ad3f6e5e3
vikash chand <vikash.chand2020@vitstudent.ac.in>
2025-02-22 19:32:40 +0530

    added popup

--- Frontend/src/Pages/LandingPage/LandingPage.jsx
+++ Frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,20 +1,28 @@
-import React,{useState, useEffect} from 'react';
-import {  FaSearchengin, FaSearch,FaUpload, FaUser, FaRegNewspaper,FaVideo,FaHandsHelping, FaMoneyCheckAlt, FaLinkedinIn } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import {
+  FaSearchengin,
+  FaSearch,
+  FaUpload,
+  FaUser,
+  FaRegNewspaper,
+  FaVideo,
+  FaHandsHelping,
+  FaMoneyCheckAlt,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { GiTeacher } from "react-icons/gi";
-import './LandingPage.css';
-import pdf from '../../assets/mis.xlsx'
-import boy from '../../assets/boy18.png'
+import "./LandingPage.css";
+import pdf from "../../assets/mis.xlsx";
+import boy from "../../assets/boy18.png";
 
-import baseUrl from '../../config';
-import AdSenseAd from '../AdSenseAd';
+import baseUrl from "../../config";
+import AdSenseAd from "../AdSenseAd";
+
 const LandingPage = () => {
-
   const [totalEntries, setTotalEntries] = useState(null);
-
   const [totalVisitors, setTotalVisitors] = useState(null);
-
-  
+  const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,14 +33,13 @@ const LandingPage = () => {
         // Assuming the response structure is { totalEntries: number }
         setTotalEntries(data.totalEntries);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         // Handle error, e.g., display an error message to the user
       }
     };
 
     fetchData();
   }, []);
-
 
   useEffect(() => {
     const fetchTotalVisitors = async () => {
@@ -43,7 +50,7 @@ const LandingPage = () => {
         // Assuming the response structure is { totalVisitors: number }
         setTotalVisitors(data.totalVisitors);
       } catch (error) {
-        console.error('Error fetching total visitors:', error);
+        console.error("Error fetching total visitors:", error);
         // Handle error, e.g., display an error message to the user
       }
     };
@@ -51,46 +58,64 @@ const LandingPage = () => {
     fetchTotalVisitors();
   }, []);
 
+  useEffect(() => {
+    const popupShown = localStorage.getItem("popupShown");
+    if (!popupShown) {
+      setShowPopup(true);
+      localStorage.setItem("popupShown", "true");
+    }
+  }, []);
 
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
     <div className="landing-page">
-    
-      <h1 className='colourchangetext'>Welcome to the MIS Previous Year Question and Faculty Review Portal</h1>
-    
-      <h2 >Exclusively For Mtech in Software Enginnering students </h2>
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <button className="close" onClick={handleClosePopup}>
+              Close
+            </button>
+            <h5>Update for 21 MIS students</h5>
+            <p>The placement companies excel has been updated.</p>
+            <a href={pdf} download>
+              Download the updated Excel file
+            </a>
+          </div>
+        </div>
+      )}
+      <h1 className="colourchangetext">
+        Welcome to the MIS Previous Year Question and Faculty Review Portal
+      </h1>
+
+      <h2>Exclusively For Mtech in Software Enginnering students </h2>
 
       <div className="feature-container">
-
-
-
         <div className="feature-description">
-        
           <ul className="feature-cards">
-        
-          
-    
+            <li className="feature-card">
+              <div className="feature-icon">
+                <FaUpload />
+              </div>
+              <div className="feature-details">
+                <h3> Upload</h3>
 
+                <p>
+                  automatically extracts the text from image hence ,user wont
+                  need to type subject name ,code faculty name, just You need to
+                  be a good at taking snaps
+                </p>
 
-          <li className="feature-card">
-          <div className="feature-icon">
-            <FaUpload />
-          </div>
-          <div className="feature-details">
-            <h3> Upload</h3>
-            
-            <p>automatically extracts the text from image hence ,user wont need to type subject name ,code faculty name, just You need to be a good at taking snaps</p>
-          
-            <button >   <Link className='lin' to="/qpupload">Upload </Link> </button>
-          
-        
-          </div>
-        </li>
-
-
-
-
-
+                <button>
+                  {" "}
+                  <Link className="lin" to="/qpupload">
+                    Upload{" "}
+                  </Link>{" "}
+                </button>
+              </div>
+            </li>
 
             <li className="feature-card">
               <div className="feature-icon">
@@ -98,107 +123,109 @@ const LandingPage = () => {
               </div>
               <div className="feature-details">
                 <h3>Search </h3>
-                <p>Search subject based on Subject code and Subject Name or Faculty Name </p>
-                <button >   <Link className='lin' to="/searchqps">Search </Link> </button>
-              
-                </div>
+                <p>
+                  Search subject based on Subject code and Subject Name or
+                  Faculty Name{" "}
+                </p>
+                <button>
+                  {" "}
+                  <Link className="lin" to="/searchqps">
+                    Search{" "}
+                  </Link>{" "}
+                </button>
+              </div>
             </li>
             <li className="feature-card">
-            <div className="feature-icon">
-            <FaMoneyCheckAlt />
-          </div>
-            <div className="feature-details">
-              <h3>Placements </h3>
-      
-              <p>Past year Placements</p>
-              <p>List of companies that came last year and their packages</p>
-                         
-  <button > <a href={pdf} download> download sheet</a>  </button>
-
-
-
+              <div className="feature-icon">
+                <FaMoneyCheckAlt />
               </div>
-          </li>
-           
-           
-          <li className="feature-card">
-          <div className="feature-icon">
-            <GiTeacher />
-          </div>
-          <div className="feature-details">
-            <h3>Faculty Review</h3>
-    
-            <p>Faculty serach module will only be active during ffcs Phases </p>
-            <button >   <Link className='lin' to="/facultydata">Review </Link> </button>
-              
-            </div>
-        </li>
+              <div className="feature-details">
+                <h3>Placements </h3>
 
-        <li className="feature-card">
-        <div className="feature-icon">
-          <FaHandsHelping />
-        </div>
-        <div className="feature-details">
-          <h3>Seniors Advice</h3>
-  
-          <p>Tips for placements by Placed seniors</p>
-          <button >   <Link className='lin' to="/placementBlogs">Review </Link> </button>
-            
-          </div>
-      </li>
+                <p>Past year Placements</p>
+                <p>List of companies that came last year and their packages</p>
 
-         
-        <li className="feature-card">
-        <div className="feature-icon">
-        <FaVideo/>
-        </div>
-        <div className="feature-details">
-       
-          <h3>Demo</h3>
-          <iframe className='video' src="https://www.youtube.com/embed/f41Y-V1nIlk?si=5AIHI2_Xrknep6Pv&amp;controls=0&amp;start=5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-          </div>
-      </li>
-      
+                <button>
+                  {" "}
+                  <a href={pdf} download>
+                    {" "}
+                    download sheet
+                  </a>{" "}
+                </button>
+              </div>
+            </li>
 
-      
-          
+            <li className="feature-card">
+              <div className="feature-icon">
+                <GiTeacher />
+              </div>
+              <div className="feature-details">
+                <h3>Faculty Review</h3>
 
-            
-           
+                <p>
+                  Faculty serach module will only be active during ffcs Phases{" "}
+                </p>
+                <button>
+                  {" "}
+                  <Link className="lin" to="/facultydata">
+                    Review{" "}
+                  </Link>{" "}
+                </button>
+              </div>
+            </li>
+
+            <li className="feature-card">
+              <div className="feature-icon">
+                <FaHandsHelping />
+              </div>
+              <div className="feature-details">
+                <h3>Seniors Advice</h3>
+
+                <p>Tips for placements by Placed seniors</p>
+                <button>
+                  {" "}
+                  <Link className="lin" to="/placementBlogs">
+                    Review{" "}
+                  </Link>{" "}
+                </button>
+              </div>
+            </li>
+
+            <li className="feature-card">
+              <div className="feature-icon">
+                <FaVideo />
+              </div>
+              <div className="feature-details">
+                <h3>Demo</h3>
+                <iframe
+                  className="video"
+                  src="https://www.youtube.com/embed/f41Y-V1nIlk?si=5AIHI2_Xrknep6Pv&amp;controls=0&amp;start=5"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </li>
           </ul>
         </div>
-
-       
       </div>
-      
-      
-      <div >
-      <h3>Total  Uploads <FaRegNewspaper/> {totalEntries !== null ? totalEntries : 'Loading...'}</h3>
-        <h3>Total Users <FaUser/> {totalVisitors !== null ? totalVisitors : 'Loading...'}  </h3>
-    
 
+      <div>
+        <h3>
+          Total Uploads <FaRegNewspaper />{" "}
+          {totalEntries !== null ? totalEntries : "Loading..."}
+        </h3>
+        <h3>
+          Total Users <FaUser />{" "}
+          {totalVisitors !== null ? totalVisitors : "Loading..."}{" "}
+        </h3>
+      </div>
 
-  </div>
-
-
-
-  <div className='dev'>
-      
-      
-      
-     
-  
-     
-    
-     
-      
+      <div className="dev"></div>
     </div>
-
-
-      </div>
-
-
   );
-}
+};
 
 export default LandingPage;
